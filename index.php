b13c5dd01c7503517627e5671641e20de8f4406d
ADMIN <morrison0899@gmail.com>
2017-03-06 17:28:09 +0000

    part 1commit

--- index.php
+++ index.php
@@ -5,20 +5,35 @@
 </head>
 <body>
 <h1>html says hi</h1>
+<form name="form" action="" method="get">
+    <input type="text" name="age" id="subject" value="Car Loan">
+</form>
 <p>
     <?php
     echo "Hello World";
+    echo "this break \n"."or this is".\n."or this is"."or";
+    echo "this is \r\n"."and we good to go";
     echo "Hello"." "."World"."!";
     echo 5*7;
     $myname="Morrison Barreto";
-    $myage=111;
-    echo "My name is" .$myname ."and my age is" .$myage;
+    echo "My name is" .$myname ."and my age is";
     echo " I get printed";
     //ia am a comment and do not get printed
     if($myname=="Morrison Barreto")
+
     {
         echo "Hi".$myname. "you are logged in";
     }
+    echo "the value you entered is";
+    echo $_GET['age'];
+    $myage=$_GET['age'];
+    echo $myage;
+    if($myage>16){
+        echo "specs";
+    }elseif ($myage<16){
+        echo"no specs";
+    }
+
     ?>
 </p>
 </body>
