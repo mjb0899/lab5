af6211c59c1dd1f877a43bfe258d4f9cd1eccecb
ADMIN <morrison0899@gmail.com>
2017-03-06 17:03:44 +0000

    part 1commit

--- index.php
+++ index.php
@@ -10,6 +10,15 @@
     echo "Hello World";
     echo "Hello"." "."World"."!";
     echo 5*7;
+    $myname="Morrison Barreto";
+    $myage=111;
+    echo "My name is" .$myname ."and my age is" .$myage;
+    echo " I get printed";
+    //ia am a comment and do not get printed
+    if($myname=="Morrison Barreto")
+    {
+        echo "Hi".$myname. "you are logged in";
+    }
     ?>
 </p>
 </body>
