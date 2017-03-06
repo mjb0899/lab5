5641e19e2e0de8b017d6db263d87d2d371a2fef0
ADMIN <morrison0899@gmail.com>
2017-03-06 23:15:05 +0000

    part 1commit

--- printer.php
+++ printer.php
@@ -4,17 +4,11 @@
     <title>hello Selector</title>
 </head>
 <body>
-<p>WHere would you like say hello?</p>
-<ul>
-    <li><a href="printer.php">earth</a></li>
-    <li><a href="printer.php">mars</a></li>
-    <li><a href="printer.php">uranus</a></li>
-</ul>
-
-
-
+<p>Syaing hello!TO</p>
 
 <?php
+
+echo"hellow".$type;
 $query = $_GET["query"];
 $type= $_GET["type"];
 echo"The query is".$cat."and the type is". $type;
