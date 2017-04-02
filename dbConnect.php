b0febc7b974bdbb587309706dd0aaca35cc194a2
ADMIN <morrison0899@gmail.com>
2017-04-02 23:49:03 +0100

    part 1commit

--- dbConnect.php
+++ dbConnect.php
@@ -1,5 +1,11 @@
-<?
-$connectstr_dbhost = 'MYSQLCONNSTR_localdb';
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 14/03/2017
+ * Time: 13:49
+ */
+$connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
 $connectstr_dbpassword = '';
@@ -12,7 +18,7 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
 if (!$db) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
